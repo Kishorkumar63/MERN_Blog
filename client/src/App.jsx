2f9494a27b1d20bfea9934afc3dbe6000a032a94
kishor kumar k <110096460+Kishorkumar63@users.noreply.github.com>
2024-02-10 10:32:24 +0530

    Create Pages and Rutes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Dashboard } from "./pages/Dashboard";
+import { Projects } from "./pages/Projects";
 
 function App() {
-  
-
   return (
-    <>
-    <h1>Kishor</h1>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+export const Projects = () => {
+  return (
+   <>
+   <h1>Project</h1>
+   </>
+  )
+}
