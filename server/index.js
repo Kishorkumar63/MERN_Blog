9dac573c48a299a0adcf398932a71ea39287be5d
Kishorkumar63 <kishorkumar200313@gmail.com>
2024-02-18 18:52:34 +0530

    add a middleware and a function to handle error

--- server/index.js
+++ server/index.js
@@ -6,13 +6,25 @@ const connectDatabase = require("./env/DB");
 
 //Routes
 const userRoute = require("./routes/userRoute");
-const authRoute=require("./routes/authRoute")
+const authRoute = require("./routes/authRoute");
 dotenv.config({ path: path.join(__dirname, "env/config.env") });
 
 connectDatabase();
+app.use(express.json());
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT, (req, rs) => {
   console.log(`server running ${process.env.PORT}`);
 });
-app.use("/api/user", userRoute);
-app.use("/api/auth", authRoute);
+
+//Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
