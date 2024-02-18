81b0aa53a62c57aa2c2fd0f54846f11d3aaf4357
Kishorkumar63 <kishorkumar200313@gmail.com>
2024-02-18 10:55:29 +0530

    connect Database

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
+const connectDatabase=require("./env/DB")
+
+
 
 dotenv.config({ path: path.join(__dirname, "env/config.env") });
+
+connectDatabase()
 
 app.listen(process.env.PORT, (req, rs) => {
   console.log(`server running ${process.env.PORT}`);
