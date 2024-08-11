f3ff875d24c0bd37ea547632c618600bac1fa865
panneer <kishorkumar200313@gmail.com>
2024-08-11 12:20:37 +0530

    finish  CallToaction and Comment section in  UI

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,8 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import CallToAction from "../compnents/CallToAction";
+import CommentSection from "../compnents/CommentSection";
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -30,52 +32,50 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
-  if(loading)return (
-    <div className="flex justify-center items-center min-h-screen">
-    <Spinner size={"xl"} color={"primary"}/>
-    </div>
-  )
+  if (loading)
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Spinner size={"xl"} color={"primary"} />
+      </div>
+    );
   return (
-    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
         {post && post.title}
       </h1>
       <Link
         to={`/search?category=${post && post.category}`}
-        className='self-center mt-5'
+        className="self-center mt-5"
       >
-        <Button color='gray' pill size='xs'>
+        <Button color="gray" pill size="xs">
           {post && post.category}
         </Button>
       </Link>
       <img
         src={post && post.image}
         alt={post && post.title}
-        className='mt-10 p-3 max-h-[600px] w-full object-cover'
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
       />
-      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className='italic'>
+        <span className="italic">
           {post && (post.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
       <div
-        className='p-3 max-w-2xl mx-auto w-full post-content'
+        className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      <div className='max-w-4xl mx-auto w-full'>
-        
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
       </div>
+      <CommentSection postId={post._id} />
 
-
-      <div className='flex flex-col justify-center items-center mb-5'>
-        <h1 className='text-xl mt-5'>Recent articles</h1>
-        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
-          
-        </div>
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center"></div>
       </div>
     </main>
- 
   );
 };
 
