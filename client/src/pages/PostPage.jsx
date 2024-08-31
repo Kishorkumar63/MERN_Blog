e6c6d9aa567aa2378d06ab9e36dcb8d254dde038
panneer <kishorkumar200313@gmail.com>
2024-08-31 10:53:04 +0530

    finished recent articles pages

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,11 +4,13 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import CallToAction from "../compnents/CallToAction";
 import CommentSection from "../compnents/CommentSection";
+import PostCard from "../compnents/PostCard";
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts,setRecentPosts]=useState(null)
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -32,6 +34,22 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -73,7 +91,10 @@ const PostPage = () => {
 
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
-        <div className="flex flex-wrap gap-5 mt-5 justify-center"></div>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+        {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
       </div>
     </main>
   );
